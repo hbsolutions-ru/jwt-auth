8e31eb10e9adba5d8d7c377bde1e42bde844db45
Igor Losev <igor.losev@gmail.com>
2021-04-13 16:34:41 +0700

    [IMPROVEMENT] Use 'jwt' query param for authentication.

--- src/Service/WebAuthorization.php
+++ src/Service/WebAuthorization.php
@@ -34,18 +34,30 @@ final class WebAuthorization
     }
 
     /**
-     * Authenticate (by the HTTP Authorization header) and authorize user
+     * Authenticate (by the HTTP Authorization header or query param) and authorize user
      *
      * @param Request $request
      */
     public function authorize(Request $request): void
     {
-        if (!preg_match("/Bearer\s+(.*)$/i", $request->getHeaderLine("Authorization"), $matches)) {
+        $jwt = null;
+
+        // Try authorization header
+        if (preg_match("/Bearer\s+(.*)$/i", $request->getHeaderLine("Authorization"), $matches)) {
+            $jwt = (string)$matches[1];
+        }
+
+        // Try query param
+        if (!$jwt && isset($request->getQueryParams()['jwt'])) {
+            $jwt = $request->getQueryParams()['jwt'];
+        }
+
+        if (empty($jwt)) {
             throw new AuthenticationException('Authorization header not found or is not in the correct format');
         }
 
         try {
-            $data = $this->jwtService->authenticate((string)$matches[1]);
+            $data = $this->jwtService->authenticate($jwt);
         } catch (\RuntimeException $e) {
             throw new AuthenticationException($e->getMessage());
         }
